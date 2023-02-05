8bba23c6294100a99f94831758d6f3f95f391bee
GTcanyan <HJ2570336418@163.com>
2023-02-06 00:13:35 +0800

    Initial commit

---
+++ src/main/java/com/hhjrp/pingju/PingjuApplication.java
@@ -0,0 +1,11 @@
+package com.hhjrp.pingju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingjuApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PingjuApplication.class, args);
+    }
+}
---
+++ src/test/java/com/hhjrp/pingju/PingjuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhjrp.pingju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingjuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
